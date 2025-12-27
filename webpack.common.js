03621e673cfe9a63151bd7bb515e10033cf700cf
Pretty-Flacko <48690875+Pretty-Flacko@users.noreply.github.com>
2025-12-27 20:25:07 +0400

    Add eslint and prettier support

--- webpack.common.js
+++ webpack.common.js
@@ -10,15 +10,15 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-        template: "./src/template.html",
+      template: "./src/template.html",
     }),
   ],
   module: {
     rules: [
-        {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-        },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
 };
